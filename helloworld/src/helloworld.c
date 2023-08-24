b447e98d7206ba30ee4b317745a545dbc423d0c0
Matan Amsalem <matan.7.8.1991@gmail.com>
2023-08-24 10:22:36 +0300

    add note

---
+++ helloworld/src/helloworld.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : helloworld.c
+ Author      : Matan Amsalem
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	printf("asdasd");
+	getwchar();
+	//asdasd
+	return EXIT_SUCCESS;
+}
